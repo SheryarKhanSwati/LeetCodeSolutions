540f47aa3af8dd50c33d4fec7a55ec7f57ac1bbe
SheryarKhan <sheryswati256@gmail.com>
2024-03-05 04:17:11 +0500

    updated solution

--- src/ReverseLinkedList1/Main.java
+++ src/ReverseLinkedList1/Main.java
@@ -2,7 +2,9 @@ package ReverseLinkedList1;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode head = new ListNode(3,null);
+        ListNode one = new ListNode(5,null);
+        ListNode head = new ListNode(3,one);
+
          Solution s = new Solution();
         System.out.println(s.reverseList(head));
     }
