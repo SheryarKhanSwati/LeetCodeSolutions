b34f5e2da14eba354fa4919d7d61dfbbf5874f11
SheryarKhan <sheryswati256@gmail.com>
2024-03-06 02:44:32 +0500

    updated solution

--- src/ReverseLinkedList1/Solution.java
+++ src/ReverseLinkedList1/Solution.java
@@ -3,19 +3,24 @@ package ReverseLinkedList1;
 public class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode currNode = head;
+        ListNode prev = null;
 
         if(head == null || head.next == null){
             return head;
         }
 
         while(currNode != null){
-            currNode = currNode.next;
+            ListNode nxt = currNode.next;
+            currNode.next = prev;
+            prev = currNode;
+
+            currNode = nxt;
         }
-        head = currNode;
+
 
 
         // head = head.next;
 
-        return head;
+        return prev;
     }
 }
