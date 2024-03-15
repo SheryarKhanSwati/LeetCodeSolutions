eef2731e3b7ffc61606cc89810646aa6ac7627b7
SheryarKhan <sheryswati256@gmail.com>
2024-03-15 15:37:32 +0500

    All posible permutation of an array solution

--- src/BackTracking/AllPermutationsOfNums.java
+++ src/BackTracking/AllPermutationsOfNums.java
@@ -1,4 +1,12 @@
 package BackTracking;
 
 public class AllPermutationsOfNums {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3};
+
+        Solution solution = new Solution();
+        System.out.println(solution.permute(arr));
+    }
+
+
 }
